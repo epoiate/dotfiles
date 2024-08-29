8f8a3ac5b206dc2995c80cf15eb7705c885a8175
Eduardo Poiate <empoiate@motorola.com>
2024-08-29 12:16:33 -0300

    Changing how I import LazyVim extras

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -3,18 +3,7 @@ return {
     opts = {
         ensure_installed = {
             "angular-language-server",
-            "chrome-debug-adapter",
-            "docker-compose-language-service",
-            "eslint-lsp",
-            "eslint_d",
-            "hadolint",
-            "js-debug-adapter",
-            "json-lsp",
             "lua-language-server",
-            "node-debug2-adapter",
-            "prettier",
-            "shfmt",
-            "stylua",
             "typescript-language-server",
         },
     },
